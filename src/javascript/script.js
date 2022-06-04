dccdbecde333eb92e55f6d63aceb460e66047872
Gabriel Sanchez <88407659+gabrielsanzs@users.noreply.github.com>
2022-06-04 05:03:29 -0300

    First commit
    Portfólio ainda não finalizado

---
+++ src/javascript/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+  $(".arrow-btn").click(function () {
+    $("")
+  });
+});
+
+var typed = new Typed(".typing", {
+  strings: ["Front-end...", "Fullstack...", "Freelancer."],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+var typed = new Typed(".typing2", {
+  strings: ["Front-end,", "Fullstack,", "Freelancer!"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
